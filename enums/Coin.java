ced8a0faf7ab1e9faf5f36df784d2f89b152cd42
Lokesh Reddy <peddamallulokeswarareddy2001@gmail.com>
2026-03-08 13:54:20 +0530

    enums with properties and methods

---
+++ enums/Coin.java
@@ -0,0 +1,15 @@
+package oopsfundamentals.enums;
+
+public enum Coin {
+    PENNY(1),
+    NICKEL(5),
+    DIME(10),
+    QUARTER(25);
+    private int value;
+    Coin(int value){
+        this.value =value;
+    }
+    public int getValue(){
+        return value;
+    }
+}
